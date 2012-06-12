bff949d2253c91777135d4410e7d597e2759514a
betuzo <betotsol@gmail.com>
2012-06-12 10:44:28 -0500

    test

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -2,7 +2,7 @@ dataSource {
     pooled = true
     driverClassName = "org.postgresql.Driver"
     username = "postgres"
-    password = "prueba"
+    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
